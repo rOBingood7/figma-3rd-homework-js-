d5c8da210fb8c86543643b44a6ddda75ac097daf
Mirkomil Mukhtorov <101549380+rOBingood7@users.noreply.github.com>
2024-04-06 18:18:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let name = prompt('Как вас зовут?').toLowerCase().trim()
 if (name === 'alex') {
     
-    let money = +prompt('Номер счета?')
-    if (money === 10000) {
+    let account = +prompt('Номер счета?')
+    if (account === 7777) {
 
-        let account = +prompt('Сколько обналичить?')
-        if (account === 7777) {
+        let money = +prompt('Сколько обналичить?')
+        if (money <= 10000) {
 
             alert('Все отлично')
             let remaining = +prompt('Сколько снял? Сколько осталось?')
